0ac276b47a0f837f0562a2b05af36cd36437c5a0
Rolfes <lucasatarc@gmail.com>
2026-06-29 13:42:06 -0500

    First Commit, Sample Webpage

---
+++ script.js
@@ -0,0 +1,4 @@
+// This file is successfully linked! 
+// You don't need it for the menu animation since CSS handles it,
+// but you can add your custom interactivity here later.
+console.log("script.js is loaded and running.");
