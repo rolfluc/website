dbcb0b6d1fe12764bfddc2ce396982e5d17a0714
Rolfes <lucasatarc@gmail.com>
2026-07-02 16:11:31 -0500

    First Entry

--- script.js
+++
@@ -1,4 +0,0 @@
-// This file is successfully linked! 
-// You don't need it for the menu animation since CSS handles it,
-// but you can add your custom interactivity here later.
-console.log("script.js is loaded and running.");
